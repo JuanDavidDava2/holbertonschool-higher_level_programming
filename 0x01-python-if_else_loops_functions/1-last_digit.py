d9693835b9d27f34848bac9719fe305aab3c0264
JuanDavidDava2 <juandm417@gmail.com>
2019-12-10 18:13:30 +0000

    first commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+if number < 0:
+    last = -last
+print("last digit of {:d} is {:d} and is".format(number, last), end=" ")
+if last > 5:
+    print("greaer than 5")
+elif last == 0:
+    print("0")
+else:
+    print("less that 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range (97, 123):
+    if i is not 113 and i is not 101:
+        print("{}".format(chr(i)), end="")
