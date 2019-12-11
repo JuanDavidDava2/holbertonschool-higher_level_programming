1bf1c0ae46460815b469678d8a413a73841e50cb
JuanDavidDava2 <juandm417@gmail.com>
2019-12-11 01:37:35 +0000

    print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 x = 0
 for i in range(ord('z'), ord('a') -1, -1):
-	print ("{}".format((chr(i - x)), end="")
+	print ("{}".format(chr(i - x)), end="")
 	x = 32 if x == 0 else 0
 
