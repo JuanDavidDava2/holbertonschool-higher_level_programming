e6451662428f761e9f4531f5ae2ddd68012e2b8d
JuanDavidDava2 <juandm417@gmail.com>
2020-01-14 03:36:13 +0000

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+'''
+prints a text with 2 new lines after each of these characters: ., ? and :
+'''
+
+
 def text_indentation(text):
+    '''
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    '''
+
+    '''check if text is a string'''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in text:
