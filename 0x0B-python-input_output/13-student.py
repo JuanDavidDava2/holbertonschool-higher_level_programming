7b938a045934e123f19c8cf258fff86ee1c04902
JuanDavidDava2 <juandm417@gmail.com>
2020-01-22 03:43:15 +0000

    task 8 to 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+class Student
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            dirt = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    dirt[item] = getattr(self, item)
+            return dirt
+
+    def reload_from_json(self, json):
+        """ function that replaces all attributes of the Student instance """
+        for key, value in json.items():
+            setattr(self, key, value)
