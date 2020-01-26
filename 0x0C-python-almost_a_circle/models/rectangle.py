5bab89cbd1429173819722a0dc2637a5cf0dbfd5
JuanDavidDava2 <juandm417@gmail.com>
2020-01-26 06:37:32 +0000

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Reactangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """property"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """property"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
