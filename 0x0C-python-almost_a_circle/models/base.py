027e03e06e3343e528ba70247f0e64bf6d49e24f
JuanDavidDava2 <juandm417@gmail.com>
2020-01-28 02:33:44 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """
 class base.
 """
+import os
+import json
 
 class Base:
 
@@ -11,5 +13,43 @@ class Base:
         if id is not None:
             self.id = id
         else: 
-            type(self).__nb_objects += 1
-            self.id = self.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        empty_list = []
+        if list_objs:
+            for i in list_objs:
+                empty_list.append(cls.to_dictionary(i))
+        with open(cls.__name__ + ".json", "w") as filename:
+            filename.write(cls.to_json_string(empty_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None
+	    return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ is "Rectangle":
+            obj = cls(1, 1)
+        elif cls.__name__ is "Square":
+            obj = cls(1)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        file = cls.__name__ + ".json"
+        if not os.path.isfile(file)
+            return []
+        with open(file, "r") as filname:
+            return [cls.create(**d) for d in cls.from_json_string(filname.read())]
