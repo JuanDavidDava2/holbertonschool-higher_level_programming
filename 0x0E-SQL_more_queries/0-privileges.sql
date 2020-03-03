dea5687391faff49819f5100467e155480d9888d
unknown <juandm417@gmail.com>
2020-03-03 17:21:10 -0500

    idk

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List all privileges of the MySQL users user_0d_1 and user_0d_2
+-- List all privileges of the MySQL users user_0d_1 and user_0d_2.
+-- in localhost.
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
