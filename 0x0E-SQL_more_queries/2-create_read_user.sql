79704fbf1ed5600376b783a0ddbd4e79d3e23e14
unknown <juandm417@gmail.com>
2020-03-03 17:26:50 -0500

    fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE IF NOT EXISTS DATABASE 'hbtn_0d_2';
+-- crate.
+CREATE USER IF NOT EXISTS  'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
