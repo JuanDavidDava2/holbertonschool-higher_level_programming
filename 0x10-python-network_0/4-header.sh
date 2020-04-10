7a85169e2f7953001cd986bb7cad5b73b84b60b8
JuanDavidDava2 <juandm417@gmail.com>
2020-04-10 01:34:36 +0000

    add flag -s

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-curl -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
