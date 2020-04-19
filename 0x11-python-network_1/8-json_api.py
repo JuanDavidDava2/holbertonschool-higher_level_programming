8aa36a9959d5668ea2c357e824ca2580b976cf87
JuanDavidDava2 <juandm417@gmail.com>
2020-04-19 21:28:42 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Task 8 Send a POST request to http://0.0.0.0:5000/search_user with letter as a
+parameter"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        q = ''
+    else:
+        q = argv[1]
+    reqst = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        data = reqst.json()
+        if data:
+            print('[{}] {}'.format(data.get('id'), data.get('name')))
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
