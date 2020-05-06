b02844ee57a6b22a894d0f1eaafec704d15522d4
JuanDavidDava2 <juandm417@gmail.com>
2020-05-06 04:03:37 +0000

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,9 @@ const arg = parseInt(process.argv[2]);
 let i;
 
 if (Number.isInteger(arg)) {
-  for (i = 0; i < arg; i++) { 
-      console.log('C is fun'); }
+  for (i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
 } else {
   console.log('Missing number of occurrences');
 }
