124f3fcda85246a64b07f69af1429ff29f6d80e9
JuanDavidDava2 <juandm417@gmail.com>
2020-05-13 04:01:13 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// movies where the character Wedge Antilles.
+const request = require('request');
+
+request.get(process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const json = JSON.parse(body);
+  let count = 0;
+  for (let i = 0; i < json.count - 2; i++) {
+    if (json.results[i].characters[18]) {
+      count += 1;
+    }
+  }
+  console.log(count);
+});
