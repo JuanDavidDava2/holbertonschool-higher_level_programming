1b9e77f7e3ccf8decaaade59dfc46332c9ec0c26
JuanDavidDava2 <juandm417@gmail.com>
2020-05-13 04:12:43 +0000

    return

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,13 @@
 const request = require('request');
 
 request.get(process.argv[2], function (err, res, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const json = JSON.parse(body);
-    let count = 0;
-    for (let i = 0; i < json.count - 2; i++) {
-      if (json.results[i].characters[18]) {
-        count += 1;
-      }
+  if (err) throw err;
+  const json = JSON.parse(body);
+  let count = 0;
+  for (let i = 0; i < json.count - 2; i++) {
+    if (json.results[i].characters[18]) {
+      count += 1;
     }
-    console.log(count);
   }
+  console.log(count);
 });
